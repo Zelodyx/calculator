918fa37ca9ce7bcf5c33f9ecb44054a285a41ff1
Zelody <zelody@Equipo1.com>
2023-03-04 18:22:52 -0600

    Hacer estructura de la calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,98 @@
 function App() {
-
-}
+  return(
+  <div>
+    <h1>Calculator</h1>
+    <hr />
+    <table style={{
+      width:'400px'}}>
+      <tr style={{border: '1px solid black'}}>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+      </tr>
+      <tr style={{border: '1px solid black'}}>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+      </tr>
+      <tr style={{border: '1px solid black'}}>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+      </tr>
+      <tr style={{border: '1px solid black'}}>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+      </tr>
+      <tr style={{border: '1px solid black'}}>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+      </tr>
+      <tr style={{border: '1px solid black'}}>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+        <td style={{border: '1px solid black'}}>
+          0
+        </td>
+      </tr>
+      
+    </table>
+  </div>
+  )
+}         
 
 export default App
